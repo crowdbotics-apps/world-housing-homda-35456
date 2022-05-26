41dbb6f15c9e9f6f299278ed4a3a071e87a79867
Crowdbotics <team@crowdbotics.com>
2022-05-26 15:55:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "world_housing_homda_35456",
+  url: "https://world_housing_homda_35456.botics.co",
+  api: "https://world_housing_homda_35456.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
